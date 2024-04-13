a756664e92e99ac453b0f6e6f7b412b776b98365
vojone <vvd1@centrum.cz>
2024-04-13 17:24:37 +0200

    Fixes in the board parsing

--- life.cpp
+++ life.cpp
@@ -4,7 +4,7 @@
  *
  */
 
-#include <mpi.h>
+//#include <mpi.h>
 
 #include <iostream>
 #include <fstream>
@@ -16,34 +16,37 @@ int parse_board(const char* input_file_path, std::vector<std::vector<uint8_t>> &
     std::ifstream input_file(input_file_path, std::ios::in);
 
     board.clear();
-    board.insert(board.begin(), std::vector<uint8_t>());
+    board.push_back(std::vector<uint8_t>());
 
     char c;
-    size_t line_cnt = 1;
+    size_t line_i = 0;
     while(!input_file.eof()) {
         input_file.get(c);
         if(input_file.eof()) {
             break;
         }
-        else if(c == '\n' || c == '\r') {
+    
+        if(c == '\n' || c == '\r') {
             if(c == '\r') {
                 input_file.get(c);
             }
 
-            if(board.size() > 1 && board[board.size() - 1].size() != board[board.size() - 2].size()) {
-                std::cerr << "line " << line_cnt << ": The input board must have rectangular shape!" << std::endl;
+            if(board[line_i].size() != board.front().size()) {
+                std::cerr << "line " << (line_i + 1);
+                std::cerr << ": The input board must have a rectangular shape!" << std::endl;
                 return 1;
             }
 
-            line_cnt++;
-            board.insert(board.begin(), std::vector<uint8_t>());
+            line_i++;
+            board.push_back(std::vector<uint8_t>({}));
         }
         else {
+            std::cerr << c << std::endl;
             if(c == '1') {
-                board[board.size() - 1].insert(board[board.size() - 1].begin(), 1);
+                board[line_i].push_back(1);
             }
             else {
-                board[board.size() - 1].insert(board[board.size() - 1].begin(), 0);
+                board[line_i].push_back(0);
             }
         }
     }
@@ -55,16 +58,29 @@ int parse_board(const char* input_file_path, std::vector<std::vector<uint8_t>> &
 
 
 int main(int argc, char **argv) {
-    if(argc < 2) {
+    if(argc < 3) {
         std::cerr << "USAGE: ./life <path-to-board> <it-num> " << std::endl;
         return 2;
     }
 
-    std::vector<std::vector<uint8_t>> board;
+    const char *board_path = argv[1];
+    size_t iteration_number = std::stoi(argv[2]);
 
-    int ret = parse_board(argv[0], board);
-    if(ret) {
-        return ret;
+    int rank = 0, size;
+    // MPI_Init (&argc, &argv);
+
+    // // Get rank and total number of processors
+    // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    // MPI_Comm_size(MPI_COMM_WORLD, &size);
+
+    if(rank == 0) { // The first (input) processor
+
+        std::vector<std::vector<uint8_t>> board;
+
+        int ret = parse_board(argv[1], board);
+        if(ret) {
+            return ret;
+        }
     }
 
     return 0;
