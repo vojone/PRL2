9b2aec04ac63e7e799d21f8be83dd95dbe2ae829
vojone <vvd1@centrum.cz>
2024-04-13 17:59:35 +0200

    Experiment with cartesian topology

--- life.cpp
+++ life.cpp
@@ -4,12 +4,24 @@
  *
  */
 
-//#include <mpi.h>
+#include <mpi.h>
 
 #include <iostream>
 #include <fstream>
 #include <vector>
 
+#define DEBUG
+
+
+#ifdef DEBUG
+    #define LOG(rank, ...)\
+        fprintf(stderr, "Rank: %d  ", rank);\
+        fprintf(stderr, __VA_ARGS__);\
+        fprintf(stderr, "\n");\
+        fflush(stderr);
+#else
+    #define LOG(...) ""
+#endif
 
 
 int parse_board(const char* input_file_path, std::vector<std::vector<uint8_t>> &board) {
@@ -41,7 +53,6 @@ int parse_board(const char* input_file_path, std::vector<std::vector<uint8_t>> &
             board.push_back(std::vector<uint8_t>({}));
         }
         else {
-            std::cerr << c << std::endl;
             if(c == '1') {
                 board[line_i].push_back(1);
             }
@@ -58,30 +69,43 @@ int parse_board(const char* input_file_path, std::vector<std::vector<uint8_t>> &
 
 
 int main(int argc, char **argv) {
-    if(argc < 3) {
-        std::cerr << "USAGE: ./life <path-to-board> <it-num> " << std::endl;
-        return 2;
-    }
+    MPI_Init (&argc, &argv);
 
-    const char *board_path = argv[1];
-    size_t iteration_number = std::stoi(argv[2]);
+    int rank = 0, size, ret = 0;
 
-    int rank = 0, size;
-    // MPI_Init (&argc, &argv);
+    // Get rank and total number of processors
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
 
-    // // Get rank and total number of processors
-    // MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-    // MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm comm;
+    int dims[1] = {size}, periodic[1] = {1}, coords[1];
+    MPI_Cart_create(MPI_COMM_WORLD, 1, dims, periodic, 0, &comm);
 
+
+    std::vector<std::vector<uint8_t>> my_cells;
     if(rank == 0) { // The first (input) processor
+        if(argc < 3) {
+            std::cerr << "USAGE: ./life <path-to-board> <it-num> " << std::endl;
+            return 2;
+        }
+
+        const char *board_path = argv[1];
+        size_t iteration_number = std::stoi(argv[2]);
 
         std::vector<std::vector<uint8_t>> board;
 
-        int ret = parse_board(argv[1], board);
-        if(ret) {
-            return ret;
-        }
+        ret = parse_board(argv[1], board);
+
+    }
+    else {
+
     }
 
-    return 0;
+    int left, right;
+    MPI_Cart_shift(comm, 0, 1, &left, &right);
+    LOG(rank, "L: %d R: %d", left, right);
+
+    MPI_Finalize();
+
+    return ret;
 }
